6d68c842fd241cd3c76ed7a0d6ffbdd3eddecb7d
Suresh9354 <suresh22cse@gmail.com>
2025-07-20 16:58:03 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,39 +1,46 @@
-import express from 'express';
-import dotenv from 'dotenv';    
-import connectDB from './config/db.js';
-import noterouters from './routes/noteroutes.js';
-import rateLimit from './middleware/rateLimiter.js';
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import noterouters from "./routes/noteroutes.js";
+import rateLimit from "./middleware/rateLimiter.js";
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-app.use(cors(
-    {
-        origin : 'http://localhost:5173',
-    }
-));
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimit);
 
-
 // Middleware to handle CORS
-// app.use((req, res, next) => {   
-    //     console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
-    //     next();
-    // });
-    
-    app.use("/api/notes", noterouters );
-    
-    
-    const PORT = process.env.PORT || 5000;
-    
-connectDB().then(() => {
+// app.use((req, res, next) => {
+//     console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+//     next();
+// });
 
-    app.listen(PORT, () => {
+app.use("/api/notes", noterouters);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
 });
 
 export default app; // Export the app for testing purposes
-
