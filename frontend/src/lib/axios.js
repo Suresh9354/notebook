a327d3324e1f402c7236a5a6f086cfcbc4840a8e
Suresh9354 <suresh22cse@gmail.com>
2025-12-17 08:37:50 +0530

    Update BASE_URL for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://notebook-0lfa.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true, // optional (use if cookies/auth)
 });
 
 export default api;
